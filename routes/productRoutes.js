075cb72aa56bc567c265632a36fad2a62638471d
PNIKITA14 <230101120005@centurionuniv.edu.in>
2026-05-04 18:28:14 +0530

    Added complete backend code

---
+++ routes/productRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/Product");
+
+// GET all products
+router.get("/", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+// ADD MULTIPLE PRODUCTS
+router.post("/bulk", async (req, res) => {
+  const products = await Product.insertMany(req.body);
+  res.json(products);
+});
+
+module.exports = router;
